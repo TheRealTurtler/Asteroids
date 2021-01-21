12211eb6474334c3b390616075ec728b6d0aad22
TheRealTurtler <michael.diedler.7399@gmail.com>
2021-01-21 14:38:52 +0100

    Added projectiles

---
+++ Asteroids/Projectile.py
@@ -0,0 +1,31 @@
+import pygame
+
+from Vector2D import Vector2D
+
+class Projectile:
+    """description of class"""
+
+    def __init__(self, pos = Vector2D(0, 0), vel = Vector2D(0, 0), size = 1) :
+        if type(pos) != Vector2D :
+            return NotImplemented
+
+        if type(vel) != Vector2D :
+            return NotImplemented
+
+        if type(size) not in (int, float) :
+            return NotImplemented
+
+        self.pos = pos
+        self.vel = vel
+        self.size = size
+        self.wrap = 0
+        self.maxWrap = 1
+
+    def update(self) :
+        self.pos += self.vel
+
+    def draw(self, screen, color = pygame.Color(255, 255, 255)) :
+        if type(color) != pygame.Color :
+            return NotImplemented
+
+        pygame.draw.circle(screen, color, (self.pos.x, self.pos.y), self.size)
