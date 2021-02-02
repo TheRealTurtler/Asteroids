640c0e15e7c6c65786809cbad16881a30c8e116a
Kai Frisch <kayfresh12@live.de>
2021-02-02 10:44:12 +0100

    Aterioden haben jetzt interessante Form

--- src/Asteroid.py
+++ src/Asteroid.py
@@ -1,5 +1,6 @@
 import pygame
-
+import math
+import random
 
 class Asteroid:
     """description of class"""
@@ -12,14 +13,24 @@ class Asteroid:
     speedMultiplierMedium = 1.5
     speedMultiplierSmall = 2
 
+    numPoints = 20
+
     def __init__(self, pos, vel = pygame.Vector2(0, 0), size = sizeBig, speedMultiplier = speedMultiplierBig):
         self.pos = pos
         self.vel = vel * speedMultiplier
         self.size = size
         self.rot = 0
 
+        self.pointOffsets = [pygame.Vector2(
+            (self.size + self.size * random.uniform(-0.2, 0.2)) * math.cos((2 * math.pi * x) / self.numPoints),
+            (self.size + self.size * random.uniform(-0.2, 0.2)) * math.sin((2 * math.pi * x) / self.numPoints)
+        ) for x in range(self.numPoints)]
+
+        self.points = [pygame.Vector2(0,0) for offset in self.pointOffsets]
+
     def update(self):
         self.pos += self.vel
+        self.points = [pygame.Vector2(self.pos + offset) for offset in self.pointOffsets]
 
     def draw(self, screen, color=pygame.Color(255, 255, 255)):
         if type(screen) != pygame.Surface:
@@ -28,4 +39,4 @@ class Asteroid:
         if type(color) != pygame.Color:
             return NotImplemented
 
-        pygame.draw.circle(screen, color, self.pos, self.size, 1)
+        pygame.draw.polygon(screen, color, self.points, 1)
